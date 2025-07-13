cc5c5a6b60689b2490b6ceaff619c8b887f94713
Sergei Zuev <zuev56@ro.ru>
2025-07-13 21:52:31 +0300

    feat: change theme in runtime

---
+++ EqualsConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace DevCardsManager;
+
+public sealed class EqualsConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value != null && value.Equals(parameter);
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,10 +1,12 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using Avalonia.Styling;
 using DevCardsManager.ViewModels;
 
 namespace DevCardsManager.Views;
 
-public partial class MainWindow : Window
+public sealed partial class MainWindow : Window
 {
     public MainWindow()
     {
@@ -18,5 +20,13 @@ public partial class MainWindow : Window
         var viewModel = (MainWindowViewModel)DataContext!;
         viewModel.AddToClipboardAsync = text => Clipboard!.SetTextAsync(text);
         viewModel.ReadClipboardAsync = () => Clipboard!.GetTextAsync();
+    }
+
+    private void ChangeThemeMenuItem_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var app = Application.Current!;
+        app.RequestedThemeVariant = app.ActualThemeVariant == ThemeVariant.Dark
+            ? ThemeVariant.Light
+            : ThemeVariant.Dark;
     }
 }
