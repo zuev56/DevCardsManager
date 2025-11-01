fd63f6286196bf70294bcf8c18b845a36133384c
Sergei Zuev <zuev56@ro.ru>
2025-11-01 21:49:51 +0300

    refactor: extract SettingsControl

---
+++ Controls/SettingsControl.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace DevCardsManager.Controls;
+
+public sealed partial class SettingsControl : UserControl
+{
+    public SettingsControl()
+    {
+        InitializeComponent();
+    }
+}
