97e213183cddb57b61f4be204f8a6767dc2f667a
Sergei Zuev <zuev56@ro.ru>
2025-11-23 14:54:34 +0300

    feat: add extended card info button

--- Ui/Controls/CardControl.axaml.cs
+++ Ui/Controls/CardControl.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.Interactivity;
 
 namespace DevCardsManager.Ui.Controls;
 
@@ -18,4 +19,14 @@ public sealed partial class CardControl : UserControl
                 viewModel.SelectedCard = cardViewModel;
         }
     }
+
+    private void InfoButton_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (InfoPopup.IsOpen)
+            InfoPopup.Close();
+        else
+            InfoPopup.Open();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e) => InfoPopup.Close();
 }
